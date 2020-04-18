96d4fed0004c1b6fcfa6f75ecab8cf35170f163e
bayuxbca <bayuxbca@gmail.com>
2020-04-19 00:59:26 +0700

    Inital commit pertama

---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>BayuXBCA</title>
+  </head>
+  <body>
+
+    <!-- Jumbotron Mulai -->
+
+    <div class="jumbotron text-center">
+        
+        <img src="img/bayu.jpg" class="rounded-circle">
+        <h2>Bayu Cakra Aditya</h2>
+        <p>Student | Programmer | Web Development</p>
+
+    </div>
+
+    <!-- Akhir Dari Jumbotron -->
+
+    <!-- About Mulai -->
+
+  <section class="about" id="about">
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-sm-12">
+
+          <h2 class="text-center">About</h2>
+
+          <hr>
+
+        </div>
+
+      </div>
+
+
+      <div class="row">
+
+        <div class="col-sm-5 offset-sm-1">
+
+          <p>Nama Saya Bayu Cakra Aditya, Saat ini berumur 20 tahun. Saya memiliki passion yang tinggi terhadap Information Technology, Web Programming, Startup Programming, Software Developer, dan Coding. </p>
+
+        </div>
+
+        <div class="col-sm-5">
+
+          <p>Saya mendalami Programming melalui otididak, walaupun tanpa adanya guru maupun pembimbing, saya berusaha belajar dan terus belajar agar bisa expert bidang Programming dan kelak dapat memberikan ilmunya kepada seluruh orang di dunia</p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+    <!-- Akhir Dari About -->
+
+    <!-- Portfolio Mulai -->
+
+    <section class="portfolio" id="portfolio">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-sm-12 text-center">
+
+            <h2>Portfolio</h2>
+
+            <hr>
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-sm-4" >
+
+            <a href="" class="thumbnail">
+
+              <img src="img/naily.jpg">
+
+            </a>
+
+          </div>
+
+          <div class="col-sm-4">
+
+            <a href="" class="thumbnail">
+
+              <img src="img/Firda.jpg">
+
+            </a>
+
+          </div>
+
+          <div class="col-sm-4">
+
+            <a href="" class="thumbnail">
+
+              <img src="img/Fitri.jpg">
+
+            </a>
+
+          </div>
+
+          <div class="col-sm-4">
+
+            <a href="" class="thumbnail">
+
+              <img src="img/Ummul.jpg">
+
+            </a>
+
+          </div>
+
+          <div class="col-sm-4">
+
+            <a href="" class="thumbnail">
+
+              <img src="img/Hana Ayu.jpg">
+
+            </a>
+
+          </div>
+
+          <div class="col-sm-4">
+
+            <a href="" class="thumbnail">
+
+              <img src="img/Ifanah.jpg">
+
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>  
+
+    <!-- Akhir Dari Portfolio -->
+
+    <!-- Contact Dimulai -->
+
+      <section class="contact" id="contact">
+
+          <div class="container">
+
+              <div class="row">
+
+                <div class="col-sm-12 text-center">
+
+                  <h2>Contact Me</h2>
+
+                  <hr>
+
+                </div>
+              
+              </div>
+
+              <div class="row">
+
+                <div class="col-sm-8 offset-sm-2">
+                  
+                  <form action="">
+                    
+                      <div class="form-group">
+                        
+                          <label for="nama">Nama</label>
+                          <input type="text" id="nama" class="form-control" placeholder="Masukkan Nama">
+
+                      </div>
+
+                      <div class="form-group">
+                        
+                          <label for="email">Email</label>
+                          <input type="email" id="email" class="form-control" placeholder="Masukkan Email">
+
+                      </div>
+
+                      <div class="form-group">
+                        
+                          <label for="telp">No.Telpon</label>
+                          <input type="tel" id="telp" class="form-control" placeholder="Masukkan Nomor Telepon">
+
+                      </div>
+
+                      <select class="form-control">
+                        <option>Pilih Kategori</option>
+                        <option>Web Development</option>
+                        <option>Web Design</option>
+                        <option>UI Design</option>
+                        <option>Software Engineering</option>
+                      </select>
+
+                      <div class="form-group">
+                        <label for="pesan">Pesan</label>
+                        <textarea class="form-control" rows="10" placeholder="Masukkan Pesan Anda Disini"></textarea>
+                      </div>
+
+                  <button type="submit" class="btn btn-primary">Kirim Pesan</button>
+
+                  </form>
+
+                </div>
+
+              </div>
+
+          </div>
+
+      </section>
+
+    <!-- Akhir Dari Contact -->
+
+    <!-- Footer Dimulai -->
+
+      <footer>
+        <div class="container text-center">
+
+          <div class="row">
+              <div class="col-sm-12">
+                <p>Copyright &copy 2020 | By. <a href="">Bayu Cakra Aditya</a></p>
+              </div>
+          </div>
+
+          <div class="row">
+            <div class="col-sm-12">
+              <a href="www.bcaland.blogspot.com" class="btn btn-danger">Kunjungi Situs Official</a>
+            </div>
+          </div>
+
+        </div>
+
+      </footer>
+
+    <!-- Akhir Dari Footer -->
+
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
